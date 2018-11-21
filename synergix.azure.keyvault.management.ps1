3d3696852d25413820a146d303f71dd0c33715cb
Sanjay Jadvani <32437715+jadvanisk@users.noreply.github.com>
2018-11-21 12:24:45 -0500

    Update synergix.azure.keyvault.management.ps1

--- synergix.azure.keyvault.management.ps1
+++ synergix.azure.keyvault.management.ps1
@@ -1,1 +1,16 @@
+#region Main
+# Create users
+# Create groups
+# Register App
+# Create Service Principal with a certificate from a Certificate Authority
+# https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-authenticate-service-principal-powershell
 
+# Assign Custom and Fine-Grained Roles, must be less than Contributor
+
+# Create Key Vault
+# Create Key
+# Create Secret
+# Encrypt Secret
+# Decrypt Secret
+
+#endregion
