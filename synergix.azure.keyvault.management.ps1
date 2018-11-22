12b73fbc20d5700d4a95ee5c0a94cb0df79b5b1d
Sanjay Jadvani <32437715+jadvanisk@users.noreply.github.com>
2018-11-22 11:03:07 -0500

    Update synergix.azure.keyvault.management.ps1

--- synergix.azure.keyvault.management.ps1
+++ synergix.azure.keyvault.management.ps1
@@ -32,6 +32,8 @@ Description
 
 # ** Task list *** 
 
+# Login to Azure Tenant account
+
 # Create Azure AD normal test user accounts
 # Create Azure AD PAM test user accounts
 # Create security groups, one dynamic and one is assigned
@@ -43,23 +45,27 @@ Description
 # https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-authenticate-service-principal-powershell
 # Can there be a servicePrincipal for each Azure AD joined device and granted fine-grained role ?
 
+# Create custom roles
+
 # Assign Custom and Fine-Grained Roles, must be less than Contributor
 # https://cmatskas.com/service-principals-in-microsoft-azure/
 
 # Create Key Vault
 # Create Key
 # Read public key
+# https://rahulpnath.com/blog/getting-started-with-azure-key-vault/
 
 # Create Secret
 # Encrypt Secret, using Public Key
 
-# Build PAWEXT machine
-# Build PAWINT machine
+# Build PAWEXT01 machine; accessible over Internet
+# Build PAWINT01 machine; accessible in Azure only
 
 # Create Firewall
 
 # Read Secret
 # Decrypt Secret
+# https://rahulpnath.com/blog/getting-started-with-azure-key-vault/
 # Example var decryptedData = await keyClient.DecryptDataAsync(keyIdentifier, "RSA_OAEP", encryptedText);
 
 # Log entry
