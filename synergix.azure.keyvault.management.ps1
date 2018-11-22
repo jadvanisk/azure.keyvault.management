94b60897eaa7ea6b804f508d0ae4e705a80a4a72
Sanjay Jadvani <32437715+jadvanisk@users.noreply.github.com>
2018-11-21 19:59:15 -0500

    Update synergix.azure.keyvault.management.ps1

--- synergix.azure.keyvault.management.ps1
+++ synergix.azure.keyvault.management.ps1
@@ -1,11 +1,13 @@
 #region Main
-# Create users
-# Create groups
+# Create normal test user accounts
+# Create PAM test user accounts
+# Create security groups, one dynamic and one is assigned
 # Register App
 # Create Service Principal with a certificate from a Certificate Authority
 # https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-authenticate-service-principal-powershell
 
 # Assign Custom and Fine-Grained Roles, must be less than Contributor
+# https://cmatskas.com/service-principals-in-microsoft-azure/
 
 # Create Key Vault
 # Create Key
