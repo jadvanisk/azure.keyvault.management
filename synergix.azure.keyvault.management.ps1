90406f6072a0ffa5923aa059f7421aa6ec7ea6ab
Sanjay Jadvani <32437715+jadvanisk@users.noreply.github.com>
2018-11-23 18:50:24 -0500

    Update synergix.azure.keyvault.management.ps1

--- synergix.azure.keyvault.management.ps1
+++ synergix.azure.keyvault.management.ps1
@@ -2,7 +2,6 @@
 .SYNOPSIS
     Some text here
     
-
 .OUTPUTS
     Some text here
 
@@ -21,7 +20,6 @@ Description
 Description
 -----------
     Some text here
-
 
 .LINK
     Some text here
@@ -45,7 +43,6 @@ Description
 # New-AzureADUser 
 # https://docs.microsoft.com/en-us/powershell/module/azuread/new-azureaduser?view=azureadps-2.0
 
-
 # $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
 # $PasswordProfile.Password = "<some randomly generated 16 character complex password string>"
 
@@ -54,7 +51,6 @@ New-AzureADUser -PasswordProfile $PasswordProfile -AccountEnabled $True -City "N
 
 # $PasswordProfile.Password = New-Guid
 New-AzureADUser -PasswordProfile $PasswordProfile -AccountEnabled $True -City "New York City" -Country "IN" -CreationType $null -Department "End User Computing" -DisplayName "Jane Smith" -GivenName "John" -surname "Smith" -JobTitle "L1 Admin - End User Computing" -MailNickName "jane@littleeinstein.me" -Mobile "+1 201 202 2030" -PhysicalDeliveryOfficeName "1200 Route 22 East" -PostalCode "08807" -State "NJ" -StreetAddress "1200 Route 22 East" -TelephoneNumber "+1 201 202 2030" -UsageLocation "IN" -UserPrincipalName "jane.smith@littleeinstein.me" -usertype "Member"
-
 
 # Create Azure AD PAM test user accounts
 # New-AzureADUser -PasswordProfile $PasswordProfile -AccountEnabled $True -City "New York City" -Country "US" -CreationType $null -Department "End User Computing" -DisplayName "John Smith" -GivenName "John" -surname "Smith" -JobTitle "L1 Admin - End User Computing" -MailNickName "john@littleeinstein.me" -Mobile "+1 201 202 2030" -PhysicalDeliveryOfficeName "1200 Route 22 East" -PostalCode "08807" -State "NJ" -StreetAddress "1200 Route 22 East" -TelephoneNumber "+1 201 202 2030" -UsageLocation "US" -UserPrincipalName "john.smith@littleeinstein.me" -usertype "Member"
@@ -73,10 +69,15 @@ New-AzureADGroup -Description "SYNERGIX Managed Azure AD Assigned PAM Users" -Di
 # Get-AzureADGroupMember
 # Add-AzureADGroupMember
 
-
 # Register Apps
 # 1. EUD
 # 2. PAM
+
+# Select-AzureRmSubscription -SubscriptionName "<yourSubscriptionName>"
+$myApplication = New-AzureRmADApplication -DisplayName "automation-app" -HomePage "https://test.com" -IdentifierUris "https://test.com" -Password "#1234a$%pwd07"
+New-AzureRmADServicePrincipal -ApplicationId $myApplication.ApplicationId
+New-AzureRmRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName $myApplication.ApplicationId
+
 
 # Create Service Principal with a certificate from a Certificate Authority
 # https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-authenticate-service-principal-powershell
